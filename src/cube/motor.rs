1435eaba02cbdbb97b413fb6ad107c510bbff5aa
Yabe.Kazuhiro <Kazuhiro.Yabe@gmail.com>
2022-04-08 18:53:30 +0900

    Update

--- src/cube/motor.rs
+++ src/cube/motor.rs
@@ -1,181 +1,180 @@
 //! Official Specification: <https://toio.github.io/toio-spec/en/docs/ble_motor>
 
 use crate::position::CubeLocation;
+use log::debug;
+use serde::ser::{SerializeSeq, Serializer};
 use serde::Serialize;
-use serde::ser::{Serializer, SerializeSeq};
 use std::error::Error;
 use std::time;
-use log::debug;
 use thiserror::Error;
 
 /// Control command
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 enum MotorCommand {
-Run,
-Period,
-TargetPosition,
-MultlTargetPositions,
-Acceleration,
+    Run,
+    Period,
+    TargetPosition,
+    MultlTargetPositions,
+    Acceleration,
 }
 
 impl From<MotorCommand> for u8 {
-fn from(cmd: MotorCommand) -> u8 {
-    match cmd {
-        MotorCommand::Run => 1u8,
-        MotorCommand::Period => 2u8,
-        MotorCommand::TargetPosition => 3u8,
-        MotorCommand::MultlTargetPositions => 4u8,
-        MotorCommand::Acceleration => 5u8,
+    fn from(cmd: MotorCommand) -> u8 {
+        match cmd {
+            MotorCommand::Run => 1u8,
+            MotorCommand::Period => 2u8,
+            MotorCommand::TargetPosition => 3u8,
+            MotorCommand::MultlTargetPositions => 4u8,
+            MotorCommand::Acceleration => 5u8,
+        }
     }
-}
 }
 
 impl Serialize for MotorCommand {
-fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-where
-    S: Serializer,
-{
-    let byte_string: u8 = u8::from(*self);
-    serializer.serialize_u8(byte_string)
-}
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let byte_string: u8 = u8::from(*self);
+        serializer.serialize_u8(byte_string)
+    }
 }
 
 /// Errors
 
 #[derive(Error, Debug, PartialEq)]
 pub enum MotorError {
-#[error("invalid parameter")]
-InvalidParameter,
-#[error("internal error of motor.rs")]
-FoundBug,
+    #[error("invalid parameter")]
+    InvalidParameter,
+    #[error("internal error of motor.rs")]
+    FoundBug,
 }
 
 /// Response type
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub enum ResponseType {
-SingleMotorControl,
-MultipleMotorControl,
-CubeSpeed,
-UnknownResponse(u8),
+    SingleMotorControl,
+    MultipleMotorControl,
+    CubeSpeed,
+    UnknownResponse(u8),
 }
 
 impl From<ResponseType> for u8 {
-fn from(response_type: ResponseType) -> u8 {
-    match response_type {
-        ResponseType::SingleMotorControl => 0x83u8,
-        ResponseType::MultipleMotorControl => 0x84u8,
-        ResponseType::CubeSpeed => 0xe0u8,
-        ResponseType::UnknownResponse(x) => x,
+    fn from(response_type: ResponseType) -> u8 {
+        match response_type {
+            ResponseType::SingleMotorControl => 0x83u8,
+            ResponseType::MultipleMotorControl => 0x84u8,
+            ResponseType::CubeSpeed => 0xe0u8,
+            ResponseType::UnknownResponse(x) => x,
+        }
     }
-}
 }
 
 impl From<u8> for ResponseType {
-fn from(num: u8) -> ResponseType {
-    match num {
-        0x83 => ResponseType::SingleMotorControl,
-        0x84 => ResponseType::MultipleMotorControl,
-        0xe0 => ResponseType::CubeSpeed,
-        x => ResponseType::UnknownResponse(x),
+    fn from(num: u8) -> ResponseType {
+        match num {
+            0x83 => ResponseType::SingleMotorControl,
+            0x84 => ResponseType::MultipleMotorControl,
+            0xe0 => ResponseType::CubeSpeed,
+            x => ResponseType::UnknownResponse(x),
+        }
     }
-}
 }
 
 impl Serialize for ResponseType {
-fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-where
-    S: Serializer,
-{
-    let byte_string: u8 = u8::from(*self);
-    serializer.serialize_u8(byte_string)
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let byte_string: u8 = u8::from(*self);
+        serializer.serialize_u8(byte_string)
+    }
 }
-}
-
 
 /// Response code from cube
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub enum ResponseCode {
-Success,
-ErrorTimeout,
-ErrorIdMissed,
-ErrorInvalidParameter,
-ErrorInvalidCubeState,
-SuccessWithOverwrite,
-ErrorNotSupported,
-ErrorFailToAppend,
-UnknownError(u8),
+    Success,
+    ErrorTimeout,
+    ErrorIdMissed,
+    ErrorInvalidParameter,
+    ErrorInvalidCubeState,
+    SuccessWithOverwrite,
+    ErrorNotSupported,
+    ErrorFailToAppend,
+    UnknownError(u8),
 }
 
 impl From<ResponseCode> for u8 {
-fn from(response_code: ResponseCode) -> u8 {
-    match response_code {
-        ResponseCode::Success => 0u8,
-        ResponseCode::ErrorTimeout => 1u8,
-        ResponseCode::ErrorIdMissed => 2u8,
-        ResponseCode::ErrorInvalidParameter => 3u8,
-        ResponseCode::ErrorInvalidCubeState => 4u8,
-        ResponseCode::SuccessWithOverwrite => 5u8,
-        ResponseCode::ErrorNotSupported => 6u8,
-        ResponseCode::ErrorFailToAppend => 7u8,
-        ResponseCode::UnknownError(x) => x,
+    fn from(response_code: ResponseCode) -> u8 {
+        match response_code {
+            ResponseCode::Success => 0u8,
+            ResponseCode::ErrorTimeout => 1u8,
+            ResponseCode::ErrorIdMissed => 2u8,
+            ResponseCode::ErrorInvalidParameter => 3u8,
+            ResponseCode::ErrorInvalidCubeState => 4u8,
+            ResponseCode::SuccessWithOverwrite => 5u8,
+            ResponseCode::ErrorNotSupported => 6u8,
+            ResponseCode::ErrorFailToAppend => 7u8,
+            ResponseCode::UnknownError(x) => x,
+        }
     }
-}
 }
 
 impl From<u8> for ResponseCode {
-fn from(binary_code: u8) -> ResponseCode {
-    match binary_code {
-        0 => ResponseCode::Success,
-        1 => ResponseCode::ErrorTimeout,
-        2 => ResponseCode::ErrorIdMissed,
-        4 => ResponseCode::ErrorInvalidParameter,
-        5 => ResponseCode::ErrorInvalidCubeState,
-        6 => ResponseCode::SuccessWithOverwrite,
-        7 => ResponseCode::ErrorNotSupported,
-        8 => ResponseCode::ErrorFailToAppend,
-        x => ResponseCode::UnknownError(x),
+    fn from(binary_code: u8) -> ResponseCode {
+        match binary_code {
+            0 => ResponseCode::Success,
+            1 => ResponseCode::ErrorTimeout,
+            2 => ResponseCode::ErrorIdMissed,
+            4 => ResponseCode::ErrorInvalidParameter,
+            5 => ResponseCode::ErrorInvalidCubeState,
+            6 => ResponseCode::SuccessWithOverwrite,
+            7 => ResponseCode::ErrorNotSupported,
+            8 => ResponseCode::ErrorFailToAppend,
+            x => ResponseCode::UnknownError(x),
+        }
     }
-}
 }
 
 impl Serialize for ResponseCode {
-fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-where
-    S: Serializer,
-{
-    let byte_string: u8 = u8::from(*self);
-    serializer.serialize_u8(byte_string)
-}
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let byte_string: u8 = u8::from(*self);
+        serializer.serialize_u8(byte_string)
+    }
 }
 
 /// Response/Notify data from cube
 
 #[derive(Debug)]
 pub struct MotorInfo {
-pub time: time::Instant,
-pub response_type: ResponseType,
-pub id: usize,
-pub response_code: ResponseCode,
+    pub time: time::Instant,
+    pub response_type: ResponseType,
+    pub id: usize,
+    pub response_code: ResponseCode,
 }
 
 /// Motor Id
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub enum MotorId {
-Left,
-Right,
+    Left,
+    Right,
 }
 
 impl From<MotorId> for u8 {
-fn from(motor_id: MotorId) -> u8 {
-    match motor_id {
-        MotorId::Left => 1u8,
-        MotorId::Right => 2u8,
+    fn from(motor_id: MotorId) -> u8 {
+        match motor_id {
+            MotorId::Left => 1u8,
+            MotorId::Right => 2u8,
+        }
     }
-}
 }
 
 impl Serialize for MotorId {
@@ -197,7 +196,7 @@ pub enum MotorDirection {
 }
 
 impl From<MotorDirection> for u8 {
-        fn from(moving_direction: MotorDirection) ->u8 {
+    fn from(moving_direction: MotorDirection) -> u8 {
         match moving_direction {
             MotorDirection::Forward => 1u8,
             MotorDirection::Backward => 2u8,
@@ -230,9 +229,7 @@ impl From<RunningPeriod> for u8 {
 
 impl RunningPeriod {
     pub fn forever() -> Self {
-        Self {
-            period: 0,
-        }
+        Self { period: 0 }
     }
 
     pub fn from_millis(perio: u64) -> Self {
@@ -241,9 +238,7 @@ impl RunningPeriod {
             1..=255 => RunningPeriod {
                 period: period as u8,
             },
-            _ => RunningPeriod {
-                period: 0,
-            },
+            _ => RunningPeriod { period: 0 },
         }
     }
 }
@@ -322,7 +317,7 @@ pub enum RotationOption {
 }
 
 impl From<RotationOption> for u8 {
-        fn from(rotation_option: RotationOption) -> u8 {
+    fn from(rotation_option: RotationOption) -> u8 {
         match rotation_option {
             RotationOption::AbsoluteOptimal => 0u8,
             RotationOption::AbsolutePositive => 1u8,
@@ -379,7 +374,6 @@ impl Serialize for MoveTarget {
         }
         seq.end()
     }
-
 }
 
 /// List of move target
@@ -436,7 +430,7 @@ pub enum WriteMode {
 }
 
 impl From<WriteMode> for u8 {
-        fn from(write_mode: WriteMode) -> u8 {
+    fn from(write_mode: WriteMode) -> u8 {
         match write_mode {
             WriteMode::Overwrite => 0u8,
             WriteMode::Append => 1u8,
@@ -615,6 +609,7 @@ struct MotorControlMultiTargetPositionsHeader {
     speed_change_type: SpeedChangeType,
     _reserved_1: u8,
     write_mode: WriteMode,
+    move_target_list: MoveTargetList,
 }
 
 /// Binary parameter representation of <https://toio.github.io/toio-spec/en/docs/ble_motor/#motor-control-with-acceleration-specified>
@@ -626,7 +621,7 @@ struct MotorControlAccleration {
     translational_speed: u8,
     acceleration: u8,
     rotation_velocity: u16,
-    rotation_direction:  RotationDirection,
+    rotation_direction: RotationDirection,
     moving_direction: MovingDirection,
     priority: Priority,
     period: RunningPeriod,
@@ -690,10 +685,10 @@ pub trait MotorBleData {
         max_speed: u8,
         speed_change_type: SpeedChangeType,
         write_mode: WriteMode,
-        cube_location: Vec<CubeLocation>,
+        move_target_list: MoveTargetList,
     ) -> Result<Vec<u8>, Box<dyn Error + Send + Sync + 'static>> {
         // create header part
-        let mut control_data = bincode::serialize(&MotorControlMultiTargetPositionsHeader {
+        let control_data = bincode::serialize(&MotorControlMultiTargetPositionsHeader {
             command: MotorCommand::MultlTargetPositions,
             id: 0xcc,
             timeout: timeout.into(),
@@ -702,12 +697,9 @@ pub trait MotorBleData {
             speed_change_type,
             write_mode,
             _reserved_1: 0xff,
+            move_target_list,
         })
         .unwrap();
-        // add body
-        for location in cube_location.iter() {
-            control_data.extend(bincode::serialize(location).unwrap());
-        }
         debug!("len: {:2}, data: {:?}", control_data.len(), control_data);
         Ok(control_data)
     }
@@ -737,7 +729,6 @@ pub trait MotorBleData {
         debug!("len: {:2}, data: {:?}", control_data.len(), control_data);
         Ok(control_data)
     }
-
 }
 
 pub trait Motor: MotorBleData {
@@ -777,13 +768,11 @@ mod test {
         assert!(test1.encode_run(10, 20, RunningPeriod::forever()).is_ok());
         assert!(test1.encode_run(-10, 20, RunningPeriod::forever()).is_ok());
         assert!(test1.encode_run(10, -20, RunningPeriod::forever()).is_ok());
-        assert!(test1.encode_run(1000, -20, RunningPeriod::forever()).is_err());
         assert!(test1
-            .encode_run(
-                10,
-                -20,
-                RunningPeriod::from_millis(200)
-            )
+            .encode_run(1000, -20, RunningPeriod::forever())
+            .is_err());
+        assert!(test1
+            .encode_run(10, -20, RunningPeriod::from_millis(200))
             .is_ok());
         assert!(test1
             .encode_run_to_target_position(
@@ -801,11 +790,12 @@ mod test {
                 32,
                 SpeedChangeType::Acceleration,
                 WriteMode::Append,
-                vec![
-                    CubeLocation::default(),
-                    CubeLocation::default(),
-                    CubeLocation::default()
-                ],
+                MoveTargetList {
+                    list: vec![MoveTarget {
+                        cube_location: CubeLocation::default(),
+                        rotation_option: RotationOption::SameAsAtWriting,
+                    },]
+                }
             )
             .is_ok());
     }
