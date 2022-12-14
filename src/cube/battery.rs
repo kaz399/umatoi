db880c43ef301f1686ec87d4a9cebc2ed93bf4fd
YABE.Kazuhiro <Kazuhiro.Yabe@gmail.com>
2022-12-14 13:35:17 +0900

    Add battery and button caracteristics

--- src/cube/battery.rs
+++ src/cube/battery.rs
@@ -1,1 +1,27 @@
+//! Official Specification:  <https://toio.github.io/toio-spec/en/docs/ble_battery>
 
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub struct BatteryInformation {
+    pub level: usize,
+}
+
+impl Default for BatteryInformation {
+    fn default() -> Self {
+        Self {
+            level: 0,
+        }
+    }
+}
+
+impl BatteryInformation {
+    pub fn new(byte_data: &[u8]) -> Option<BatteryInformation> {
+        if byte_data.is_empty() {
+            return None;
+        }
+        Some(BatteryInformation{
+            level: byte_data[1].into(),
+        }
+        )
+    }
+}
--- src/cube/button.rs
+++ src/cube/button.rs
@@ -3,23 +3,58 @@
 use std::time;
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
-pub enum Button {
-    Unknown,
+pub enum ButtonState {
+    Unknown(u8),
     Pressed,
     Released,
 }
 
-#[derive(Debug, Copy, Clone)]
-pub struct ButtonInfo {
-    pub time: time::Instant,
-    pub button: Button,
-}
-
-impl Default for ButtonInfo {
-    fn default() -> Self {
-        Self {
-            time: time::Instant::now(),
-            button: Button::Unknown,
+impl From<ButtonState> for u8 {
+    fn from(state: ButtonState) -> Self {
+        match state {
+            ButtonState::Pressed => 0x80u8,
+            ButtonState::Released => 0x00u8,
+            ButtonState::Unknown(x) => x,
         }
     }
 }
+
+impl From<u8> for ButtonState {
+    fn from(code: u8) -> ButtonState {
+        match code {
+            0x80u8 => ButtonState::Pressed,
+            0x00u8 => ButtonState::Released,
+            x => ButtonState::Unknown(x),
+        }
+    }
+}
+
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub struct ButtonInformation {
+    pub time: time::Instant,
+    pub state: ButtonState,
+}
+
+impl Default for ButtonInformation {
+    fn default() -> Self {
+        Self {
+            time: time::Instant::now(),
+            state: ButtonState::Unknown(0xffu8),
+        }
+    }
+}
+
+impl ButtonInformation {
+    pub fn new(byte_data: &[u8]) -> Option<ButtonInformation> {
+        if byte_data.len() < 2 {
+            return None;
+        }
+        match byte_data[0] {
+            0x01u8 => Some(ButtonInformation { time: time::Instant::now(), state: ButtonState::from(byte_data[1]) }),
+            _ => None,
+        }
+    }
+}
+
+
